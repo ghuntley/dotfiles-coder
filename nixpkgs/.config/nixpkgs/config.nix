7c9f2f0239c494432057d70ea9f86f35e99d3f7b
Geoffrey Huntley <ghuntley@ghuntley.com>
2023-03-22 00:09:47 +1100

    Update config.nix

--- nixpkgs/.config/nixpkgs/config.nix
+++ nixpkgs/.config/nixpkgs/config.nix
@@ -1,1 +1,5 @@
-{ allowUnfree = true; }
+{ 
+  allowUnfree = true;
+  substituters = https://cache.nixos.org https://cachix.cachix.org https://devenv.cachix.org
+  trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM= devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+}
